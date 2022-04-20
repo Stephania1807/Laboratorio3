3bf1b30d40f17d0c23f6b661efb3cc335b03497a
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-04-20 02:47:11 -0500

    new servicios

--- src/main/java/com/example/laboratorio3/Controllers/ServicioController.java
+++ src/main/java/com/example/laboratorio3/Controllers/ServicioController.java
@@ -1,14 +1,17 @@
 package com.example.laboratorio3.Controllers;
 
-import com.example.laboratorio3.Repository.CuentaRepository;
-import com.example.laboratorio3.Repository.MascotaRepository;
-import com.example.laboratorio3.Repository.ResponsableRepository;
-import com.example.laboratorio3.Repository.ServicioRepository;
+import com.example.laboratorio3.Entity.Mascota;
+import com.example.laboratorio3.Entity.OpcionServicio;
+import com.example.laboratorio3.Entity.Servicio;
+import com.example.laboratorio3.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/servicios")
@@ -26,15 +29,46 @@ public class ServicioController {
     @Autowired
     ResponsableRepository responsableRepository;
 
+    @Autowired
+    OpcionServicioRepository opcionServicioRepository;
+
     @GetMapping(value = {"","/"})
     public String listaServicios(Model model){
         model.addAttribute("listaServicios", servicioRepository.findAll());
         model.addAttribute("listaMascotas", mascotaRepository.findAll());
         model.addAttribute("listaCuentas", cuentaRepository.findAll());
         model.addAttribute("listaResponsables", responsableRepository.findAll());
+        model.addAttribute("listaOpcionesServicios", opcionServicioRepository.findAll());
+
         return "servicios/lista";
     }
 
+    @GetMapping("/new")
+    public String nuevoServicio(Model model, @ModelAttribute("servicio") Servicio servicio){
+        model.addAttribute("listaMascotas", mascotaRepository.findAll());
+        model.addAttribute("listaCuentas", cuentaRepository.findAll());
+        model.addAttribute("listaResponsables", responsableRepository.findAll());
+        return "servicios/newForm";
+    }
 
+    @PostMapping("/save")
+    public String guardarInventario(Servicio servicio) {
+        servicioRepository.save(servicio);
+        return "redirect:/servicios";
+    }
+
+    @GetMapping(value = "/editar")
+    public String editarMascota(Model model, @ModelAttribute("servicio") Servicio servicio,@RequestParam("id") Integer id){
+        Optional<Servicio> optional= servicioRepository.findById(id);
+        if (optional.isPresent()){
+            model.addAttribute("servicio",optional.get());
+            model.addAttribute("listaMascotas", mascotaRepository.findAll());
+            model.addAttribute("listaCuentas", cuentaRepository.findAll());
+            model.addAttribute("listaResponsables", responsableRepository.findAll());
+            return "servicios/newForm";
+        }
+
+        return "redirect:/servicios";
+    }
 
 }
