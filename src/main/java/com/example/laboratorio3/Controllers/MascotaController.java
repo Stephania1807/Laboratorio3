652ce6d4de4c883d18f614c8f227198c5ae85de9
Stephania <stephania.guzman@pucp.edu.pe>
2022-04-19 20:13:34 -0500

    MascotaController lista

--- src/main/java/com/example/laboratorio3/Controllers/MascotaController.java
+++ src/main/java/com/example/laboratorio3/Controllers/MascotaController.java
@@ -1,9 +1,30 @@
 package com.example.laboratorio3.Controllers;
 
+import com.example.laboratorio3.Repository.MascotaRepository;
+import com.example.laboratorio3.Repository.RazaRepository;
+import com.example.laboratorio3.Repository.ServicioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mascota")
 public class MascotaController {
+    @Autowired
+    MascotaRepository mascotaRepository;
+
+    @Autowired
+    RazaRepository razaRepository;
+
+    @Autowired
+    ServicioRepository servicioRepository;
+    @GetMapping(value = {"","/"})
+    public String listaEmployee(Model model){
+        model.addAttribute("listaMascota", mascotaRepository.findAll());
+        model.addAttribute("listaRaza", razaRepository.findAll());
+        model.addAttribute("listaServicio", servicioRepository.findAll());
+        return "Mascota/lista";
+    }
 }
