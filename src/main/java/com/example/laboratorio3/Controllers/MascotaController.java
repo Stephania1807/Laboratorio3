a952a035fd1f86ce648f39508672fdaaf2a1ed18
Diego Vargas <a20191027@pucp.edu.pe>
2022-04-19 20:13:34 -0500

    pagina principal

--- src/main/java/com/example/laboratorio3/Controllers/MascotaController.java
+++ src/main/java/com/example/laboratorio3/Controllers/MascotaController.java
@@ -1,9 +1,20 @@
 package com.example.laboratorio3.Controllers;
 
+import com.example.laboratorio3.Repository.MascotaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mascota")
 public class MascotaController {
+
+
+
+
+    public String nuevaMascota(Model model){
+        return "Mascota/new";
+    }
+
 }
--- src/main/java/com/example/laboratorio3/Repository/MascotaRepository.java
+++ src/main/java/com/example/laboratorio3/Repository/MascotaRepository.java
@@ -1,5 +1,6 @@
 package com.example.laboratorio3.Repository;
 
+import com.example.laboratorio3.Entity.Mascota;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
